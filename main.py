7d0fe7ecbdef4ac373c042ac0d3b6249ae28964c
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-01-31 20:22:59 +0800

    Training decision tree classification model and predict test set result

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score,confusion_matrix
 
 dataset = pd.read_csv("Social_Network_Ads.csv")
 X = dataset.iloc[:, :-1].values
@@ -11,4 +13,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_s
 
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_train)
+X_test = sc.transform(X_test)
+
+classifier = DecisionTreeClassifier(random_state=0)
+classifier.fit(X_train, y_train)
+
+print(classifier.predict(sc.transform([[19, 19000]])))
+
+y_pred = classifier.predict(X_test)
+
+print(confusion_matrix(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
